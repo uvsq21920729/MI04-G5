38b0a34ab33913695c0e7f27f6fdace0e8e1c72a
uvsq21920729 <71814586+uvsq21920729@users.noreply.github.com>
2022-05-05 16:59:03 +0200

    Update projet_boulier.py

--- projet_boulier.py
+++ projet_boulier.py
@@ -11,10 +11,10 @@ def callback(event):
         if canvas.itemconfig(ovals[0][0][0]+a)["tags"][-1] == "current":
             n = a
             break
-    if n>0:
+    if n >= 0:
         mouvement(n)
 def mouvement(n):
-    print(ovals[n//5][n%5][1], n)
+    print(ovals[n//5][n%5][1], n, n%5)
     if ovals[n//5][n%5][1] == 0:
         if n%5 == 0:
             canvas.move(ovals[n//5][n%5][0],0,15)
@@ -39,7 +39,7 @@ def move():
     pass
         
 
-canvas = Canvas(root, width=1400, height=300)
+canvas = Canvas(root, width=1400, height=320)
 canvas.grid()
 canvas.create_rectangle(0, 0, 1400, 300, fill = "#3e280f")
 canvas.create_rectangle(10, 10, 1390, 290, fill = "#bbbbbb", outline = "#bbbbbb")
@@ -49,9 +49,10 @@ ovals = []
 for a in range(23):
     if (a+1)%2 == 0:
         pass
-    ovals.append([[canvas.create_oval(15+60*a, 10, 15+60*a+44, 54, fill = "#dd8624", state = "normal", activefill = "black"),0]])
+    ovals.append([[canvas.create_oval(15+60*a, 10, 15+60*a+44, 54, fill = "#b36d1e", state = "normal", activefill = "black"),0]])
     for b in range(4):
-        ovals[a].append([canvas.create_oval(15+60*a, 114+b*44, 15+60*a+44, 114+b*44+44, fill = "#dd8624", state = "normal", activefill = "black"),0])
+        ovals[a].append([canvas.create_oval(15+60*a, 114+b*44, 15+60*a+44, 114+b*44+44, fill = "#b36d1e", state = "normal", activefill = "black"),0])
+
 canvas.bind("<Key>", key)
 canvas.bind("<Button-1>", callback)
 canvas.pack()
