83007738c2ccd92a9af4d3a27a98fca7e8353e3d
Krankfr <67122432+Krankfr@users.noreply.github.com>
2022-05-09 06:55:35 +0200

    stuck on a bug
    the function "save_or_load()" finally works 
    now, i'm stuck with "operation()" that fucked up T_T
    i don't understand how operation fucked up move() :/
    except a "print(ovals)" i didn't do anything

--- projet_boulier.py
+++ projet_boulier.py
@@ -1,8 +1,8 @@
 ##################################################
 # groupe MI TD04                                 #
-# Bonnardot  Arthure                             #
-# Guo  Xu Xin                                    #
-# Le Brun  Maxence                               #
+# Bonnardot  Arthure                     22106864#
+# Guo  Xu Xin                            21920729#
+# Le Brun  Maxence                       22101714#
 # https://github.com/uvsq21920729/MI04-G5        #
 ##################################################
 from tkinter import *
@@ -10,7 +10,8 @@ from tkinter import *
 tickspeed = 40
 askedRange = 23
 encounter = 1
-
+ovals_bis = []
+op = []
 root = Tk()
 
 def key(event):
@@ -33,7 +34,33 @@ def reset():
                 if ovals[a][b][2]:
                     ovals[a][b][2] = 65 - ovals[a][b][2]
 def save_or_load():
-    pass
+    global ovals,ovals_bis,askedRange,encounter
+    if ovals_bis:
+        if len(ovals)*len(ovals[0])!=len(ovals_bis):
+            askedRange = len(ovals_bis)//5
+            canvas.delete('all')
+            canvas.create_rectangle(0, 0, 1400, 400, fill = "#3e280f")
+            canvas.create_rectangle(10, 10, 1390, 390, fill = "#bbbbbb", outline = "#bbbbbb")
+            canvas.create_rectangle(10, 120, 1390, 144, fill = "#3e280f")
+            encounter += 2
+            ovals = []
+            for a in range(askedRange):
+                ovals.append([[canvas.create_oval(1380/askedRange*a+15+int((23-askedRange)*a*0.3), 10, 1380/askedRange*a+15+44+int((23-askedRange)*a*0.3), 54, fill = "#b36d1e", state = "normal", activefill = "black"),0,0,0]])
+                for b in range(4):
+                    ovals[a].append([canvas.create_oval(1380/askedRange*a+15+int((23-askedRange)*a*0.3), 210+b*44, 1380/askedRange*a+15+44+int((23-askedRange)*a*0.3), 210+b*44+44, fill = "#b36d1e", state = "normal", activefill = "black"),0,0])
+        for a in range(len(ovals)):
+            for b in range(len(ovals[a])):
+                if ovals[a][b][1]!=ovals_bis[a*5+b]:
+                    if ovals_bis[a*5+b] == 3:
+                        ovals[a][b][1] = 1
+                    if ovals_bis[a*5+b] == 0:
+                        ovals[a][b][1] = 2
+        ovals_bis = []
+    else:
+        for a in range(len(ovals)):
+            for b in range(len(ovals[a])):
+                ovals_bis.append(ovals[a][b][1])
+    
 def speed():
     global tickspeed
     if tickspeed%2:
@@ -84,6 +111,7 @@ def mouvement(n):
                     ovals[N//5][N%5][1] = 2
 def move():
     global ovals,tickspeed
+    print(ovals)
     for a in range(len(ovals)):
         for b in range(len(ovals[a])):
             if ovals[a][b][2] >= 65:
@@ -107,7 +135,75 @@ def move():
                     canvas.move(ovals[a][b][0],0,1)
                     ovals[a][b][2] += 1
     canvas.after(int(tickspeed),move)
-
+def operation():
+    global askedRange,ovals,encounter,op
+    root_bis = Tk()
+    root_bis.title('Mode Opératoire')
+    root_bis.geometry('400x200')
+    op = ["0","+"]
+    def add():
+        global op
+        op = [str(eval(op[0]+op[1]+str(name_Tf.get()))),"+"]
+        name_Tf.delete(0,len(name_Tf.get()))
+    def sub():
+        global op
+        op = [str(eval(op[0]+op[1]+str(name_Tf.get()))),"-"]
+        name_Tf.delete(0,len(name_Tf.get()))
+    def mul():
+        global op
+        op = [str(eval(op[0]+op[1]+str(name_Tf.get()))),"*"]
+        name_Tf.delete(0,len(name_Tf.get()))
+    def div():
+        global op
+        op = [str(eval(op[0]+op[1]+str(name_Tf.get()))),"/"]
+        name_Tf.delete(0,len(name_Tf.get()))
+    def show():
+        global askedRange,ovals,encounter,op
+        op = eval(op[0]+op[1]+str(name_Tf.get()))
+        askedRange = len(str(op))
+        canvas.delete('all')
+        canvas.create_rectangle(0, 0, 1400, 400, fill = "#3e280f")
+        canvas.create_rectangle(10, 10, 1390, 390, fill = "#bbbbbb", outline = "#bbbbbb")
+        canvas.create_rectangle(10, 120, 1390, 144, fill = "#3e280f")
+        encounter += 2
+        ovals = []
+        for a in range(askedRange):
+            ovals.append([[canvas.create_oval(1380/askedRange*a+15+int((23-askedRange)*a*0.3), 10, 1380/askedRange*a+15+44+int((23-askedRange)*a*0.3), 54, fill = "#b36d1e", state = "normal", activefill = "black"),0,0,0]])
+            for b in range(4):
+                ovals[a].append([canvas.create_oval(1380/askedRange*a+15+int((23-askedRange)*a*0.3), 210+b*44, 1380/askedRange*a+15+44+int((23-askedRange)*a*0.3), 210+b*44+44, fill = "#b36d1e", state = "normal", activefill = "black"),0,0])
+        x = [1]
+        alterne = [2,5]
+        n = 1
+        while op%(n*alterne[-1]) != op:
+            n*=alterne[-1]
+            x.append(n)
+            alterne.reverse()
+        op = list(str(op))
+        for a in range(len(op)):
+            #ovals[a][int(op[0])%5][1]=1
+            print(int(op[0])%5)
+            if int(op[0])%5:
+                for b in range(int(op[0])%5):
+                    ovals[a][b+1][1]=1
+            if int(op[0])//5 > 0:
+                ovals[-1][0][1]=1
+            op.pop(0)
+        print(ovals)
+        close()
+    def close():
+        root_bis.destroy()
+    Label(root_bis, text="Opération a efféctuer").pack()
+    name_Tf = Entry(root_bis)
+    name_Tf.pack()
+    
+    Button(root_bis, text = " + ", command = add).pack()
+    Button(root_bis, text = " - ", command = sub).pack()
+    Button(root_bis, text = " x ", command = mul).pack()
+    Button(root_bis, text = " / ", command = div).pack()
+    Button(root_bis, text = " = ", command =show).pack()
+    
+    root_bis.mainloop()
+    
 canvas = Canvas(root, width=1400, height=400)
 canvas.grid()
 
@@ -117,6 +213,7 @@ menu_bar.add_command(label = "Vitesse", command = speed)
 menu_bar.add_command(label = "Save/Load", command = save_or_load)
 menu_bar.add_command(label = "Réinitialiser", command = reset)
 menu_bar.add_command(label = "Nbrs collones", command = space)
+menu_bar.add_command(label = "Mode Opératoire", command = operation)
 
 root.config(menu=menu_bar)
 ovals = []
@@ -129,9 +226,8 @@ for a in range(askedRange):
     for b in range(4):
         ovals[a].append([canvas.create_oval(15+60*a, 210+b*44, 15+60*a+44, 210+b*44+44, fill = "#b36d1e", state = "normal", activefill = "black"),0,0])
 
-move()
-
 canvas.bind("<Key>", key)
 canvas.bind("<Button-1>", callback)
 canvas.pack()
 root.mainloop()
+move()
